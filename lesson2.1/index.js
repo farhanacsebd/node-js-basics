1bdd635dcf4485fdebacf49eba68b615ecf5064c
farhanacsebd <farhana.cse.bd@gmail.com>
2023-08-05 23:05:30 +0600

    os module

---
+++ lesson2.1/index.js
@@ -0,0 +1,9 @@
+// os 
+// OS module provides information about the computer's operating system. 
+const os = require('os');
+console.log(os);
+console.log(os.userInfo());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(totalmem());
+console.log(freemem());
